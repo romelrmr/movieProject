86ad1f3cb65eca899021a4735e3e6e7645c6fc57
Romel Sanares <romelrmr123@gmail.com>
2026-08-02 22:03:00 -0400

    8/2/26

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 API: "https://www.omdbapi.com/?i=tt3896198&apikey=7f9d3fad&s=Harry"
+
+
+async function main() {
+    const users = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=7f9d3fad&s=Harry");
+    const usersData = await users.json();
+    console.log(
+        usersData.map(
+            (user) => `<section class="movieList">
+        <div class="movie-list__container">
+            <h3>Movie</h3>
+                <p>Title</p>
+                <p>Year></p>
+                <p>imdbID</p>
+                <p>Type</p>
+                <p>Poster</p>
+        </div>
+    </section>`)
+    );
+}
+
+main();
