d943bf4272364f6f0ed086133960ad261cba6545
Romel Sanares <romelrmr123@gmail.com>
2026-08-03 23:10:12 -0400

    8/3/26

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-API: "https://www.omdbapi.com/?i=tt3896198&apikey=7f9d3fad&s=Harry"
+API: "https://www.omdbapi.com/?i=tt3896198&apikey=7f9d3fad&s=Harry%20Potter"
 
 
 async function main() {
-    const users = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=7f9d3fad&s=Harry");
-    const usersData = await users.json();
-    console.log(
-        usersData.map(
-            (user) => `<section class="movieList">
-        <div class="movie-list__container">
-            <h3>Movie</h3>
-                <p>Title</p>
-                <p>Year></p>
-                <p>imdbID</p>
-                <p>Type</p>
-                <p>Poster</p>
-        </div>
-    </section>`)
-    );
+    const movies = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=7f9d3fad&s=Harry%20Potter");
+    const moviesData = await movies.json();
+    const movieListEl = document.querySelector('.movieList');
+    console.log(moviesData);
+    
+    movieListEl.innerHTML = moviesData.Search.map((user) => movieHTML(user)).join("");
 }
 
 main();
+
+function movieHTML(user) {
+    return `<div class="movieList">
+    <div class="movie-list__container">
+        <p><img src="${user.Poster}"</p>
+        <p>${user.Title}</p>
+        <p>${user.Year}</p>       
+    </div>
+</div>`;
+}
