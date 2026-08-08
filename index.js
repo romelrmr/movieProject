fd9605efe0d708399e924a7ad98fd655b921f536
Romel Sanares <romelrmr123@gmail.com>
2026-08-08 15:12:18 -0400

    8/8/26

--- index.js
+++ index.js
@@ -5,12 +5,9 @@ async function main() {
     const movies = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=7f9d3fad&s=Harry%20Potter");
     const moviesData = await movies.json();
     const movieListEl = document.querySelector('.movieList');
-    console.log(moviesData);
     
     movieListEl.innerHTML = moviesData.Search.map((user) => movieHTML(user)).join("");
 }
-
-main();
 
 function movieHTML(user) {
     return `<div class="movieList">
@@ -20,4 +17,33 @@ function movieHTML(user) {
         <p>${user.Year}</p>       
     </div>
 </div>`;
+}
+
+const searchInput = document.getElementById("searchInput");
+const searchBtn = document.getElementById("searchBtn");
+
+searchBtn.addEventListener("click", () => {
+    const title = searchInput.value.trim();
+
+    console.log("Searching for:", title);
+
+    if(title !== "") {
+        searchMovies(title);
+    }
+});
+
+async function searchMovies(title) {
+    const movies = await fetch(
+        `https://www.omdbapi.com/?i=tt3896198&apikey=7f9d3fad&s=${(title)}`
+    );
+    const moviesData = await movies.json();
+    console.log(moviesData);
+    const movieListEl = document.querySelector(".movieList");
+    if (moviesData.Search) {
+        movieListEl.innerHTML = moviesData.Search
+            .map((movie) => movieHTML(movie))
+            .join("");
+    }   else {
+        movieListEl.innerHTML = "<p>No movies found</p>";
+    }
 }
