2d9787634f2fc2c1763402ec19f8efe604c4bd3b
Romel Sanares <romelrmr123@gmail.com>
2026-08-12 19:49:58 -0400

    Scroll to Right and sort by A-Z

--- index.js
+++ index.js
@@ -6,18 +6,30 @@ async function main() {
     const moviesData = await movies.json();
     const movieListEl = document.querySelector('.movieList');
     
-    movieListEl.innerHTML = moviesData.Search.map((user) => movieHTML(user)).join("");
+    movieListEl.innerHTML = moviesData.Search.map((movie) => movieHTML(user)).join("");
 }
 
-function movieHTML(user) {
-    return `<div class="movieList">
+let movies = [];
+
+function movieHTML(movie) {
+    return `
     <div class="movie-list__container">
-        <p><img src="${user.Poster}"</p>
-        <p>${user.Title}</p>
-        <p>${user.Year}</p>       
-    </div>
-</div>`;
+        <img src="${movie.Poster}"</p>
+        <p>${movie.Title}</p>
+        <p>${movie.Year}</p>       
+    </div>`
+    ;
 }
+
+function displayMovies(movieArray) {
+    const movieListEl = document.querySelector(".movieList");
+    if (movieArray.length > 0) {
+        movieListEl.innerHTML = movieArray
+        .map(movie => movieHTML(movie))
+        .join("");
+    } else {movieListEl.innerHTML = "<p>No movies found</p>";
+        }
+    }
 
 const searchInput = document.getElementById("searchInput");
 const searchBtn = document.getElementById("searchBtn");
@@ -25,25 +37,44 @@ const searchBtn = document.getElementById("searchBtn");
 searchBtn.addEventListener("click", () => {
     const title = searchInput.value.trim();
 
-    console.log("Searching for:", title);
-
     if(title !== "") {
         searchMovies(title);
     }
 });
 
+
 async function searchMovies(title) {
-    const movies = await fetch(
+    const response = await fetch(
         `https://www.omdbapi.com/?i=tt3896198&apikey=7f9d3fad&s=${(title)}`
     );
-    const moviesData = await movies.json();
-    console.log(moviesData);
-    const movieListEl = document.querySelector(".movieList");
+    const moviesData = await response.json();
+
     if (moviesData.Search) {
-        movieListEl.innerHTML = moviesData.Search
-            .map((movie) => movieHTML(movie))
-            .join("");
-    }   else {
-        movieListEl.innerHTML = "<p>No movies found</p>";
+        movies = moviesData.Search;
+        
+        displayMovies(movies);
+    } else {
+        movies = [];
+        displayMovies(movies);
     }
 }
+
+const sortMovies = document.getElementById("sortMovies");
+sortMovies.addEventListener("change", () => {
+    console.log("Selected:", sortMovies.value);
+
+    if (sortMovies.value === "titleAZ") {
+        
+        const sortedMovies = [...movies];
+            sortedMovies.sort((a, b) => {
+            return a.Title.localeCompare(b.Title);
+        });
+        console.log("Sorted:", sortedMovies.map(movie => movie.Title)
+        );
+
+        displayMovies(sortedMovies);
+        }
+        else {
+            displayMovies(movies);
+        }
+    });
